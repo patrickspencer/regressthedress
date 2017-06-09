93fd14936b0d9dc5e2892d7f1fc120a077920b7b
Patrick Spencer <pkspenc@gmail.com>
2017-06-09 08:31:45 -0400

    Update main engine file

--- wombat/engine/main.py
+++ wombat/engine/main.py
@@ -1,20 +1,46 @@
 import numpy as np
 import jellyfish
 from wombat.models import dbsession, Item
+from sqlalchemy.dialects import postgresql
 
-def get_top_n_similar_items(input_text, results_num = 10):
+def get_top_n_similar_items(input_text,
+        brand, 
+        item_type, 
+        method, 
+        results_num = 10):
     """
     :param target_items: sqlalchemy objects with title attr
-    :return: a dictionary with entries of the form item_id: similarity_rating
+    :return: a tuple (l, q) where l is a list of sqlalchemy objects order from highest relevance to lowest relevance and q is the sql query that was used to search for these items
     """
 
-    items = dbsession.query(Item).filter(Item.item_type == 'dresses').all()
+    items = dbsession.query(Item).filter(
+            Item.brand == brand, 
+            Item.item_type == item_type)
+    if not items:
+        items = dbsession.query(Item).filter(
+                Item.item_type == item_type)
+
+    # we don't know which of the above outcomes will work so we'll save
+    # the actual sql query to see where we got the information from.
+    # actual_sql_query = items.statement.compile(dialect=postgresql.dialect())
 
     # compare distance of input_string to each item title
     d = {}
-    for item in items:
+
+    for item in items.all():
         similarity_rating = jellyfish.levenshtein_distance(input_text, item.title)
-        # similarity_rating = fuzz.token_sort_ratio(input_text, item.title)
+        if not method or method == 'l':
+            similarity_rating = jellyfish.levenshtein_distance(input_text, item.title)
+        elif method == 'dl': 
+            similarity_rating = jellyfish.damerau_levenshtein_distance(input_text, item.title)
+        elif method == 'j': 
+            similarity_rating = jellyfish.jaro_distance(input_text, item.title)
+        elif method == 'jw': 
+            similarity_rating = jellyfish.jaro_winkler(input_text, item.title)
+        elif method == 'h': 
+            similarity_rating = jellyfish.hamming_distance(input_text, item.title)
+
+        # keep track of items with highest relevance in a dict d
         if len(d) <= results_num:
             d[item.id] = similarity_rating
         if max(d.values()) > similarity_rating:
@@ -32,6 +58,7 @@ def get_top_n_similar_items(input_text, results_num = 10):
     for i in list_of_ids:
         list_of_objects.append(dbsession.query(Item).filter(Item.id == i[0]).first())
 
+    # return (new_l, actual_sql_query)
     return list_of_objects
 
 def get_tight_avg(l):
@@ -45,21 +72,4 @@ def get_tight_avg(l):
     for i in l:
         if i > mean - stddev and i < mean + stddev:
             new_l.append(i)
-    # return np.mean(new_l, 0)
-    return new_l
-
-l = [398.0, 350.0, 179.0, 850.0, 224.0, 395.0, 247.99, 385.0, 208.0, 190.0, 189.0]
-stddev = np.std(l)
-print(np.std(l, 0))
-mean = np.mean(l, 0)
-print(np.mean(l))
-new_l = []
-for i in l:
-    if i > mean - stddev and i < mean + stddev:
-        new_l.append(i)
-print(new_l)
-print([i / 10 for i in new_l])
-
-    
-
-input_text = "Gown with Long Sleeves and Deep V"
+    return new_l 
