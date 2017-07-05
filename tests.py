a9785222e6143bd20f47ea97031ef7e1ba10acc8
Patrick Spencer <pkspenc@gmail.com>
2017-07-05 15:02:33 -0400

    Refactor engine.__init__ funcs into one class 'Prediction'

--- tests.py
+++ tests.py
@@ -1,7 +1,9 @@
 import unittest
+import numpy
 
 # from wombat.engine.predict_price import get_predicted_value
 from wombat.engine.one_hot_funcs import one_hot_form_input
+from wombat.engine import Prediction
 
 class TestStringMethods(unittest.TestCase):
 
@@ -15,6 +17,29 @@ class TestStringMethods(unittest.TestCase):
         self.assertEqual(sample['dresses'], 1)
         self.assertEqual(sample['Tibi'], 1)
         self.assertEqual(sample['lurex'], 1)
+
+class TestPrediction(unittest.TestCase):
+
+    def test_prediction(self):
+        brand = 'Tibi'
+        item_type = 'dresses'
+        cost = 300
+        title = 'lurex evening dress'
+        prediction = Prediction(brand = brand, item_type = item_type, est_price = cost, title = title)
+        self.assertEqual(prediction.brand, 'Tibi')
+        self.assertEqual(prediction.item_type, 'dresses')
+        self.assertEqual(prediction.est_price, 300)
+        self.assertEqual(prediction.title, 'lurex evening dress')
+        self.assertEqual(prediction.one_hot_array['cost'], 300)
+        self.assertEqual(prediction.one_hot_array['dresses'], 1)
+        self.assertEqual(prediction.one_hot_array['Tibi'], 1)
+        self.assertEqual(prediction.one_hot_array['lurex'], 1)
+        # this next one is important. We cannot accidentally change the length of the 
+        # input array into the model
+        self.assertEqual(prediction.sample_len, prediction.one_hot_array_len)
+        self.assertTrue(isinstance(prediction.predicted_price,
+            numpy.float64))
+        self.assertTrue(prediction.predicted_price>0)
 
 if __name__ == '__main__':
     unittest.main()
